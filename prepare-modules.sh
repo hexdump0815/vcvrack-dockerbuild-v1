652dbe3bc30a5c3fa095f6fd5b21402ffea392fe
hexdump <hexdump0815@googlemail.com>
2019-07-04 19:23:04 +0200

    add all os modules currently in the library

--- prepare-modules.sh
+++ prepare-modules.sh
@@ -282,3 +282,445 @@ if [ -f ../../../cf.$MYARCH.patch ]; then
   patch -p1 < ../../../cf.$MYARCH.patch
 fi
 cd ..
+
+# Alikins-rack-plugins
+echo Alikins-rack-plugins
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/alikins/Alikins-rack-plugins
+cd Alikins-rack-plugins
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../Alikins-rack-plugins.$MYARCH.patch ]; then
+  patch -p1 < ../../../Alikins-rack-plugins.$MYARCH.patch
+fi
+cd ..
+
+# FrozenWasteland
+echo FrozenWasteland
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/almostEric/FrozenWasteland
+cd FrozenWasteland
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../FrozenWasteland.$MYARCH.patch ]; then
+  patch -p1 < ../../../FrozenWasteland.$MYARCH.patch
+fi
+cd ..
+
+# BaconPlugs
+echo BaconPlugs
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/baconpaul/BaconPlugs
+cd BaconPlugs
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../BaconPlugs.$MYARCH.patch ]; then
+  patch -p1 < ../../../BaconPlugs.$MYARCH.patch
+fi
+cd ..
+
+# catro-modulo
+echo catro-modulo
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/catronomix/catro-modulo
+cd catro-modulo
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../catro-modulo.$MYARCH.patch ]; then
+  patch -p1 < ../../../catro-modulo.$MYARCH.patch
+fi
+cd ..
+
+# modules chortlinghamster
+echo modules chortlinghamster
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/chortlinghamster/modules
+cd modules
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../modules.$MYARCH.patch ]; then
+  patch -p1 < ../../../modules.$MYARCH.patch
+fi
+cd ..
+
+# Bark
+echo Bark
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/Coirt/Bark
+cd Bark
+git checkout v1.0.3
+git submodule update --init --recursive
+if [ -f ../../../Bark.$MYARCH.patch ]; then
+  patch -p1 < ../../../Bark.$MYARCH.patch
+fi
+cd ..
+
+# VCVRackPlugins countmodula
+echo VCVRackPlugins countmodula
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/countmodula/VCVRackPlugins
+cd VCVRackPlugins
+git checkout V1
+git submodule update --init --recursive
+if [ -f ../../../VCVRackPlugins.$MYARCH.patch ]; then
+  patch -p1 < ../../../VCVRackPlugins.$MYARCH.patch
+fi
+cd ..
+
+# modular80
+echo modular80
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/cschol/modular80
+cd modular80
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../modular80.$MYARCH.patch ]; then
+  patch -p1 < ../../../modular80.$MYARCH.patch
+fi
+cd ..
+
+# ModularFungi
+echo ModularFungi
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/david-c14/ModularFungi
+cd ModularFungi
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../ModularFungi.$MYARCH.patch ]; then
+  patch -p1 < ../../../ModularFungi.$MYARCH.patch
+fi
+cd ..
+
+# dBiz
+echo dBiz
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/dBiz/dBiz
+cd dBiz
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../dBiz.$MYARCH.patch ]; then
+  patch -p1 < ../../../dBiz.$MYARCH.patch
+fi
+cd ..
+
+# DHE-Modules
+echo DHE-Modules
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/dhemery/DHE-Modules
+cd DHE-Modules
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../DHE-Modules.$MYARCH.patch ]; then
+  patch -p1 < ../../../DHE-Modules.$MYARCH.patch
+fi
+cd ..
+
+# moDllz
+echo moDllz
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/dllmusic/moDllz
+cd moDllz
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../moDllz.$MYARCH.patch ]; then
+  patch -p1 < ../../../moDllz.$MYARCH.patch
+fi
+cd ..
+
+# Ohmer-Modules
+echo Ohmer-Modules
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/DomiKamu/Ohmer-Modules
+cd Ohmer-Modules
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../Ohmer-Modules.$MYARCH.patch ]; then
+  patch -p1 < ../../../Ohmer-Modules.$MYARCH.patch
+fi
+cd ..
+
+# vcvrack-encoders
+echo vcvrack-encoders
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/expertsleepersltd/vcvrack-encoders
+cd vcvrack-encoders
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../vcvrack-encoders.$MYARCH.patch ]; then
+  patch -p1 < ../../../vcvrack-encoders.$MYARCH.patch
+fi
+cd ..
+
+# FrankBussRackPlugin
+echo FrankBussRackPlugin
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/FrankBuss/FrankBussRackPlugin
+cd FrankBussRackPlugin
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../FrankBussRackPlugin.$MYARCH.patch ]; then
+  patch -p1 < ../../../FrankBussRackPlugin.$MYARCH.patch
+fi
+cd ..
+
+# VCVRack-Holon.ist
+echo VCVRack-Holon.ist
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/hdavid/VCVRack-Holon.ist
+cd VCVRack-Holon.ist
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../VCVRack-Holon.ist.$MYARCH.patch ]; then
+  patch -p1 < ../../../VCVRack-Holon.ist.$MYARCH.patch
+fi
+cd ..
+
+# AmalgamatedHarmonics
+echo AmalgamatedHarmonics
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/jhoar/AmalgamatedHarmonics
+cd AmalgamatedHarmonics
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../AmalgamatedHarmonics.$MYARCH.patch ]; then
+  patch -p1 < ../../../AmalgamatedHarmonics.$MYARCH.patch
+fi
+cd ..
+
+# vcv-karatesnoopy
+echo vcv-karatesnoopy
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/KarateSnoopy/vcv-karatesnoopy
+cd vcv-karatesnoopy
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../vcv-karatesnoopy.$MYARCH.patch ]; then
+  patch -p1 < ../../../vcv-karatesnoopy.$MYARCH.patch
+fi
+cd ..
+
+# Koralfx-Modules
+echo Koralfx-Modules
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/koralfx/Koralfx-Modules
+cd Koralfx-Modules
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../Koralfx-Modules.$MYARCH.patch ]; then
+  patch -p1 < ../../../Koralfx-Modules.$MYARCH.patch
+fi
+cd ..
+
+# WhatTheRack
+echo WhatTheRack
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/korfuri/WhatTheRack
+cd WhatTheRack
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../WhatTheRack.$MYARCH.patch ]; then
+  patch -p1 < ../../../WhatTheRack.$MYARCH.patch
+fi
+cd ..
+
+# hetrickcv
+echo hetrickcv
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/mhetrick/hetrickcv
+cd hetrickcv
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../hetrickcv.$MYARCH.patch ]; then
+  patch -p1 < ../../../hetrickcv.$MYARCH.patch
+fi
+cd ..
+
+# strong_kar
+echo strong_kar
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/mrletourneau/strong_kar
+cd strong_kar
+git checkout v1.1.0
+git submodule update --init --recursive
+if [ -f ../../../strong_kar.$MYARCH.patch ]; then
+  patch -p1 < ../../../strong_kar.$MYARCH.patch
+fi
+cd ..
+
+# VCV-Rack-Plugins
+echo VCV-Rack-Plugins
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/mschack/VCV-Rack-Plugins
+cd VCV-Rack-Plugins
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../VCV-Rack-Plugins.$MYARCH.patch ]; then
+  patch -p1 < ../../../VCV-Rack-Plugins.$MYARCH.patch
+fi
+cd ..
+
+# noobhour_modules
+echo noobhour_modules
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/NicolasNeubauer/noobhour_modules
+cd noobhour_modules
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../noobhour_modules.$MYARCH.patch ]; then
+  patch -p1 < ../../../noobhour_modules.$MYARCH.patch
+fi
+cd ..
+
+# rcm-modules
+echo rcm-modules
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/Rcomian/rcm-modules
+cd rcm-modules
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../rcm-modules.$MYARCH.patch ]; then
+  patch -p1 < ../../../rcm-modules.$MYARCH.patch
+fi
+cd ..
+
+# vcv-mosquito
+echo vcv-mosquito
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/rmosquito/vcv-mosquito
+cd vcv-mosquito
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../vcv-mosquito.$MYARCH.patch ]; then
+  patch -p1 < ../../../vcv-mosquito.$MYARCH.patch
+fi
+cd ..
+
+# skylights-vcv
+echo skylights-vcv
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/Skrylar/skylights-vcv
+cd skylights-vcv
+git checkout rack-v1
+git submodule update --init --recursive
+if [ -f ../../../skylights-vcv.$MYARCH.patch ]; then
+  patch -p1 < ../../../skylights-vcv.$MYARCH.patch
+fi
+cd ..
+
+# Via-for-Rack
+echo Via-for-Rack
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/starlingcode/Via-for-Rack
+cd Via-for-Rack
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../Via-for-Rack.$MYARCH.patch ]; then
+  patch -p1 < ../../../Via-for-Rack.$MYARCH.patch
+fi
+cd ..
+
+# vcvrack-packone
+echo vcvrack-packone
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/stoermelder/vcvrack-packone
+cd vcvrack-packone
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../vcvrack-packone.$MYARCH.patch ]; then
+  patch -p1 < ../../../vcvrack-packone.$MYARCH.patch
+fi
+cd ..
+
+# quantal-audio
+echo quantal-audio
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/sumpygump/quantal-audio
+cd quantal-audio
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../quantal-audio.$MYARCH.patch ]; then
+  patch -p1 < ../../../quantal-audio.$MYARCH.patch
+fi
+cd ..
+
+# VCV-Recorder
+echo VCV-Recorder
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/VCVRack/VCV-Recorder
+cd VCV-Recorder
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../VCV-Recorder.$MYARCH.patch ]; then
+  patch -p1 < ../../../VCV-Recorder.$MYARCH.patch
+fi
+cd ..
+
+# sonusmodular
+echo sonusmodular
+# this is the version i used this script last with
+#git checkout xy
+git clone https://gitlab.com/sonusdept/sonusmodular
+cd sonusmodular
+git checkout master
+git submodule update --init --recursive
+if [ -f ../../../sonusmodular.$MYARCH.patch ]; then
+  patch -p1 < ../../../sonusmodular.$MYARCH.patch
+fi
+cd ..
+
+# 21kHz-rack-plugins
+echo 21kHz-rack-plugins
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/hexdump0815/21kHz-rack-plugins
+cd 21kHz-rack-plugins
+git checkout v1
+git submodule update --init --recursive
+if [ -f ../../../21kHz-rack-plugins.$MYARCH.patch ]; then
+  patch -p1 < ../../../21kHz-rack-plugins.$MYARCH.patch
+fi
+cd ..
+
+# Bidoo
+echo Bidoo
+# this is the version i used this script last with
+#git checkout xy
+git clone https://github.com/sebastien-bouffier/Bidoo
+cd Bidoo
+git checkout v1.0
+git submodule update --init --recursive
+if [ -f ../../../Bidoo.$MYARCH.patch ]; then
+  patch -p1 < ../../../Bidoo.$MYARCH.patch
+fi
+cd ..
