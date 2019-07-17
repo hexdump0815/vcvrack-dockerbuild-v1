7be0ab798f86f37a9a09ec6fe816076675908f0d
hexdump <hexdump0815@googlemail.com>
2019-07-17 18:27:55 +0200

    include better 21khz v1 port

--- prepare-modules.sh
+++ prepare-modules.sh
@@ -719,11 +719,11 @@ if [ -f ../../../sonusmodular.$MYARCH.patch ]; then
 fi
 cd ..
 
-# 21kHz-rack-plugins
+# 21kHz-rack-plugins - alternative v1 port with polyphony
 echo 21kHz-rack-plugins
 # this is the version i used this script last with
 #git checkout xy
-git clone https://github.com/hexdump0815/21kHz-rack-plugins
+git clone https://github.com/stephanepericat/21kHz-rack-plugins
 cd 21kHz-rack-plugins
 git checkout v1
 git submodule update --init --recursive
@@ -731,6 +731,19 @@ if [ -f ../../../21kHz-rack-plugins.$MYARCH.patch ]; then
   patch -p1 < ../../../21kHz-rack-plugins.$MYARCH.patch
 fi
 cd ..
+
+# # 21kHz-rack-plugins - my started v1 port
+# echo 21kHz-rack-plugins
+# # this is the version i used this script last with
+# #git checkout xy
+# git clone https://github.com/hexdump0815/21kHz-rack-plugins
+# cd 21kHz-rack-plugins
+# git checkout v1
+# git submodule update --init --recursive
+# if [ -f ../../../21kHz-rack-plugins.$MYARCH.patch ]; then
+#   patch -p1 < ../../../21kHz-rack-plugins.$MYARCH.patch
+# fi
+# cd ..
 
 # Bidoo
 echo Bidoo
