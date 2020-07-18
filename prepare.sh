e5f29211271334f5829dfbbe63155531b8094bae
hexdump0815 <hexdump0815@googlemail.com>
2020-07-18 07:55:38 +0200

    new simde repo location

--- prepare.sh
+++ prepare.sh
@@ -31,12 +31,10 @@ if [ "$MYARCH" == "armv7l" ] || [ "$MYARCH" == "aarch64" ]; then
     ( cd .. ; tar xzf source/simde.tar.gz )
   # ... otherwise get it from git and create a source archive afterwards
   else
-    git clone https://github.com/nemequ/simde.git
+    git clone https://github.com/simd-everywhere/simde.git
     cd simde
     # this is the version i used this script last with
-    git checkout 11f1383067d7696370c1f9cf9b50b73b6c503cb3
-    # old special branch version
-    #git checkout a61e88057c90ceb4b0b2cf5182919717bbb0496b
+    #git checkout 7d35c4411dcd1422ff3ee5fb1e0b3386c48a170f
     ( cd ../.. ; mkdir -p source ; tar czf source/simde.tar.gz compile/simde )
     cd ..
   fi
@@ -46,6 +44,7 @@ fi
 if [ -f ../source/Rack-source.tar.gz ]; then
   echo "INFO: using sources from the source archive"
   ( cd .. ; tar xzf source/Rack-source.tar.gz )
+  cd Rack
 # ... otherwise get it from git and create a source archive afterwards
 else
   git clone https://github.com/VCVRack/Rack.git
